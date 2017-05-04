60878e3c214003f98546fb3008eaeb3b4abd9b94
Thales Castello Branco <tha_branco@hotmail.com>
2017-05-04 08:30:41 -0300

    Update cadastrando.php

--- rabbit-PHP-Cadastro/php/cadastrando.php
+++ rabbit-PHP-Cadastro/php/cadastrando.php
@@ -3,7 +3,7 @@ $nome = $_POST['NomeCliente'];
 $sobrenome = $_POST['SobrenomeCliente'];
 $sexo = $_POST['Sexo'];
 
-$strcon = mysqli_connect('localhost','fabio','123','banco_teste') or die('Erro ao conectar ao banco de dados');
+$strcon = mysqli_connect('localhost','usuario','senha','database') or die('Erro ao conectar ao banco de dados');
 $sql = "INSERT INTO cadastro VALUES ";
 $sql .= "('$nome', '$sobrenome', '$sexo')"; 
 mysqli_query($strcon,$sql) or die("Erro ao tentar cadastrar registro");
